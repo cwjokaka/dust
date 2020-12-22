1b1f0fb7c7bb6f1646a9a3a8b2c370d120818414
tanjl <1250217678@qq.com>
2020-12-22 19:58:17 +0800

    场景修改

--- dust-core/src/main/java/com/dust/core/scene/AbstractScene.java
+++ dust-core/src/main/java/com/dust/core/scene/AbstractScene.java
@@ -2,6 +2,49 @@ package com.dust.core.scene;
 
 public abstract class AbstractScene implements Scene {
 
+    private String name;
 
+    public AbstractScene(String name) {
+        this.name = name;
+    }
 
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void terminate() {
+
+    }
 }
